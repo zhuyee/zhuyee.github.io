f66aaf541e77da30716138440f57cd99f7a43a36
zhuyee <3546385042@qq.com>
2020-05-24 14:56:50 +0800

    Site updated: 2020-05-24 14:56:44

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -1,8 +1,8 @@
 // 评论issues仓库 by.removeif https://removeif.github.io/
 var repoIssuesUrl = "https://api.github.com/repos/userName/userRepo/issues";
 // 对应仓库 clientId、clientSecret 关于这两个参数的安全问题，查看 https://removeif.github.io/2019/09/19/博客源码分享.html#1-热门推荐，最新评论：
-var clientId;// = "46a9f3481b46ea0129d8";
-var clientSecret;// = "79c7c9cb847e141757d7864453bcbf89f0655b24";
+var clientId = "7acfc3158ea536dba83a";
+var clientSecret = "f2ce22be66cbe81cf38bb822722313d6344d1496";
 // var authorizationToken = 'Basic ' + btoa(clientId + ':' + clientSecret);
 // 写comment count值
 var reqCommentCountUrl;
